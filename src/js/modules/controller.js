2e9f0c2375c8c400c4f453798479ce520ee52af1
juzQrios <dj30c.1@gmail.com>
2019-11-03 21:02:48 +0530

    Add Model methods to fetch weather data

--- src/js/modules/controller.js
+++ src/js/modules/controller.js
@@ -5,11 +5,14 @@ class Controller {
     this.view = view
   }
 
-  init () {
+  async init () {
     console.log('MVC up and running')
     const API_KEY = this.view.init()
     this.model.init(API_KEY)
     console.log(this.model.API_KEY)
+    const weatherData = await this.model.fetchWeatherByLocation('Tokyo, JP')
+    const processedWeatherData = this.model.processWeatherData(weatherData)
+    console.log(processedWeatherData)
   }
 }
 
--- src/js/modules/model.js
+++ src/js/modules/model.js
@@ -7,6 +7,25 @@ class Model {
     this.API_KEY = API_KEY
     localStorage.API_KEY = this.API_KEY
   }
+
+  async fetchWeatherByLocation (location) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${this.API_KEY}`)
+    const weatherData = await response.json()
+    return weatherData
+  }
+
+  processWeatherData (weatherData) {
+    console.log(weatherData)
+    const processedWeatherData = {
+      coord: weatherData.coord,
+      main: weatherData.main,
+      country: weatherData.sys.country,
+      cityName: weatherData.name,
+      cityID: weatherData.sys.id,
+      weather: weatherData.weather
+    }
+    return processedWeatherData
+  }
 }
 
 export default Model
